0a85a47a6d0e59e3bd46041b93dcd1784f2d6d95
0x36D76289 <71098898+0x36D76289@users.noreply.github.com>
2025-07-22 19:14:51 +0200

    chore: init

---
+++ src/game/direction.rs
@@ -0,0 +1,20 @@
+use glam::Vec2;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    pub fn to_vec2(self) -> Vec2 {
+        match self {
+            Direction::Up => Vec2::new(0.0, -1.0),
+            Direction::Down => Vec2::new(0.0, 1.0),
+            Direction::Left => Vec2::new(-1.0, 0.0),
+            Direction::Right => Vec2::new(1.0, 0.0),
+        }
+    }
+}
