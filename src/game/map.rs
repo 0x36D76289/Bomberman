9ce823212c224e19a76413d4d6ff95354c676820
Hack3900 <hack3900@milhax.com>
2025-07-25 20:20:04 +0200

    modified map to have a better default

--- src/game/map.rs
+++ src/game/map.rs
@@ -24,7 +24,7 @@ pub struct Map {
 impl Map {
     pub fn new(width: u8, height: u8) -> Self {
         let mut content: Vec<MapElement> =
-            vec![MapElement::Empty; width as usize * height as usize];
+            vec![MapElement::Breakable; width as usize * height as usize];
         for x in 0..width {
             content[x as usize] = MapElement::Unbreakable;
             content[width as usize * (height - 1) as usize + x as usize] = MapElement::Unbreakable;
@@ -33,6 +33,27 @@ impl Map {
             content[y as usize * width as usize] = MapElement::Unbreakable;
             content[(y + 1) as usize * width as usize - 1] = MapElement::Unbreakable;
         }
+        for y in (0..height).step_by(2) {
+            for x in (0..width).step_by(2) {
+                content[y as usize * width as usize + x as usize] = MapElement::Unbreakable;
+            }
+        }
+        // Top left corner
+        content[(1 * width) as usize + 1 as usize] = MapElement::Empty;
+        content[(1 * width) as usize + 2 as usize] = MapElement::Empty;
+        content[(2 * width) as usize + 1 as usize] = MapElement::Empty;
+        // Top right corner
+        content[(1 * width) as usize + (width - 2) as usize] = MapElement::Empty;
+        content[(1 * width) as usize + (width - 3) as usize] = MapElement::Empty;
+        content[(2 * width) as usize + (width - 2) as usize] = MapElement::Empty;
+        // Bottom left corner
+        content[((height - 2) * width) as usize + 1 as usize] = MapElement::Empty;
+        content[((height - 2) * width) as usize + 2 as usize] = MapElement::Empty;
+        content[((height - 3) * width) as usize + 1 as usize] = MapElement::Empty;
+        // Bottom right corner
+        content[((height - 2) * width) as usize + (width - 2) as usize] = MapElement::Empty;
+        content[((height - 2) * width) as usize + (width - 3) as usize] = MapElement::Empty;
+        content[((height - 3) * width) as usize + (width - 2) as usize] = MapElement::Empty;
         Map {
             width: width as usize,
             height: height as usize,
@@ -54,6 +75,6 @@ impl Map {
 
 impl Default for Map {
     fn default() -> Self {
-        Map::new(16, 16)
+        Map::new(15, 15)
     }
 }
