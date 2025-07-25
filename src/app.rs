2198786576012b7fbea5a4ad0d1cac6d31df5963
senseisamy <samy.naji2003@gmail.com>
2025-07-25 14:09:21 +0200

    vulkan triangle

---
+++ src/app.rs
@@ -0,0 +1,61 @@
+use crate::game::state::State;
+use crate::graphics::{RenderContext, Vulkan};
+use std::error::Error;
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, EventLoop},
+    keyboard::KeyCode,
+    window::WindowId,
+};
+
+pub struct App {
+    pub state: State,
+    pub vulkan: Vulkan,
+    pub rcx: Option<RenderContext>,
+}
+
+impl App {
+    pub fn new(event_loop: &EventLoop<()>) -> Result<Self, Box<dyn Error>> {
+        Ok(Self {
+            state: State::default(),
+            vulkan: Vulkan::init(event_loop)?,
+            rcx: None,
+        })
+    }
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        self.rcx = Some(RenderContext::init(event_loop, &self.vulkan).unwrap());
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
+        // TODO: main loop
+        // TODO: render
+
+        // Event Handling
+        match event {
+            WindowEvent::CloseRequested => {
+                event_loop.exit();
+            }
+            WindowEvent::RedrawRequested => {
+                self.draw_frame();
+                let rcx = self.rcx.as_mut().unwrap();
+                rcx.window.request_redraw();
+            }
+            WindowEvent::KeyboardInput { event, .. } => {
+                #[cfg(debug_assertions)]
+                if event.state.is_pressed() && event.repeat == false {
+                    if event.physical_key == KeyCode::Space {
+                        self.state.print();
+                    }
+                    if event.physical_key == KeyCode::Escape {
+                        event_loop.exit();
+                    }
+                }
+            }
+            _ => (),
+        }
+    }
+}
