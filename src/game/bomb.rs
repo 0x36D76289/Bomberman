35ecb4ffc34b7ae9a892155436ace5c9ed51f4f0
Milan ADUAKA <maduaka@f1r12s19.clusters.42paris.fr>
2025-07-30 19:28:37 +0200

    added bomb collisions, enabled tick function for bombs

--- src/game/bomb.rs
+++ src/game/bomb.rs
@@ -1,10 +1,20 @@
-use glam::Vec2;
+use glam::{usize, Vec2};
+
+use crate::game::player::Player;
 
 pub enum BombState {
     // TODO: Define the states of a bomb
     Planted,
     // Sliding(Direction),
     Exploding,
+}
+
+#[derive(Default)]
+struct Explosion {
+    up: u8,
+    down: u8,
+    left: u8,
+    right: u8,
 }
 
 pub struct Bomb {
@@ -14,6 +24,7 @@ pub struct Bomb {
     pub owner_id: u32,
     pub state: BombState,
     pub collision_enabled: bool,
+    pub explosion: Explosion,
 }
 
 const BOMB_TIMER_DEFAULT: f32 = 3.0;
@@ -31,11 +42,19 @@ impl Bomb {
             owner_id: owner,
             state: BombState::Planted,
             collision_enabled: false,
+            explosion: Explosion::default(),
         }
     }
 
-    pub fn tick(&self) {
-        // if owner not colliding and collision disabled
-        // -> enable collision
+    fn enable_collision(&mut self, players: &Vec<Player>) {
+        if self.collision_enabled {
+            return;
+        }
+        if !players[self.owner_id as usize].is_colliding(self.position, 0.5) {
+            self.collision_enabled = true;
+        }
+    }
+    pub fn tick(&mut self, players: &Vec<Player>) {
+        self.enable_collision(players);
     }
 }
