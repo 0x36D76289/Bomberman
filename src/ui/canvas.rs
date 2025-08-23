88e838949c7e5335101db62502d61bbd9eb48c4d
SenseiSamy <samy.naji2003@gmail.com>
2025-08-23 12:03:09 +0200

    wrote documentation for the Canvas struct

--- src/ui/canvas.rs
+++ src/ui/canvas.rs
@@ -8,16 +8,24 @@ use vulkano::{
 
 #[derive(Debug, Default, Clone)]
 /// Represents a rectangle canvas that can be drawn on the screen.\
-/// It has coordinates in screen space (between -1 and 1) and a RGBA color.\
-/// It can also optionally have a text that will be centered and a texture that will replace its color
+/// Can optionally have a text that will be rendered at the center of the canvas.\
+/// Can optionally have a texture that will replace the color of the canvas.\
 pub struct Canvas {
+    /// Center of the canvas (between \[-1, -1\] and \[1, 1\]).
     pub center: Vec2,
+    /// Width of the canvas (between -1 and 1).
     pub width: f32,
+    /// Height of the canvas (between -1 and 1).
     pub height: f32,
+    /// Color of the canvas (RGBA format).
     pub color: Vec4,
+    /// The index of the canvas texture.
     pub texture: Option<TextureIndex>,
+    /// The text that will be rendered inside the canvas.
     pub text: Option<String>,
+    /// The text color (RGBA format).
     pub text_color: Option<Vec4>,
+    /// The size of the text, for reference the default size is 1.
     pub text_size: Option<f32>,
 }
 
@@ -41,6 +49,7 @@ impl Canvas {
         ]
     }
 
+    /// Creates a vertex buffer for the canvas rectangle
     pub fn into_vertex_buffer(
         &self,
         memory_allocator: Arc<StandardMemoryAllocator>,
@@ -71,6 +80,7 @@ impl Canvas {
         .unwrap()
     }
 
+    /// Creates the push constant for the canvas rectangle
     pub fn push_constant(&self) -> GuiPush {
         match self.texture {
             Some(index) => GuiPush {
