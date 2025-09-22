dc3aae3b2decedeac2fbca8aa3908546dacda5db
Moutard <loic.cozdenmat@gmail.com>
2025-09-22 13:43:28 +0200

    Added entity methods

--- src/game/ai/entity.rs
+++ src/game/ai/entity.rs
@@ -2,9 +2,9 @@ use glam::Vec2;
 
 use crate::game::game_state::GameState;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum EntityType {
-    Player(i32),
+    Player(usize),
     Bomb(usize),
     Explosion,
     Powerup(Vec2),
@@ -16,4 +16,36 @@ pub struct Entity {
     pub entity_type: EntityType,
 }
 
-impl Entity {}
+impl PartialEq for Entity {
+    fn eq(&self, other: &Self) -> bool {
+        self.entity_type == other.entity_type
+    }
+}
+
+impl Entity {
+    pub fn new(entity_type: EntityType) -> Self {
+        Entity {
+            position: Vec2::new(0.0, 0.0),
+            entity_type,
+        }
+    }
+
+    pub fn get_id(self) -> usize {
+        match self.entity_type {
+            EntityType::Player(id) => id,
+            EntityType::Bomb(id) => id,
+            EntityType::Explosion => 0,
+            EntityType::Powerup(Vec2) => 0,
+        }
+    }
+
+    pub fn get_players_from_list(entities: &[Entity]) -> Vec<usize> {
+        entities
+            .iter()
+            .flat_map(|entity| match entity.entity_type {
+                EntityType::Player(id) => Some(id),
+                _ => None,
+            })
+            .collect()
+    }
+}
