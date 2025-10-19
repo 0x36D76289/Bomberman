37b4a18c6232be5ca15088fcd8b580b66a295cea
Moutard <loic.cozdenmat@gmail.com>
2025-10-19 09:29:26 +0200

    Accessible cells in zone (cells that are not blocked by bombs)

--- src/game/ai/zone.rs
+++ src/game/ai/zone.rs
@@ -2,18 +2,20 @@ use std::collections::HashSet;
 
 use crate::game::ai::ai::AI;
 use crate::game::ai::entity::Entity;
+use crate::game::bomb::Bomb;
 use crate::game::collision::Collision;
 use crate::game::map::map::Map;
 use crate::game::player::Player;
 use crate::game::powerup::PowerUp;
 use crate::game::{ai::entity::EntityType, game_state::GameState};
-use crate::utils::vec2::ApproxEq;
+use crate::utils::vec2::{ApproxEq, Grid};
 use glam::Vec2;
 #[derive(Debug, Clone, Default)]
 /// Zone is a collection of empty cells, used mostly for the AI
 /// it contains also a vector of entities
+
 pub struct Zone {
-    pub cells: Vec<Vec2>,
+    cells: Vec<Vec2>,
     pub entities: Vec<Entity>,
     player_zone: bool,
 }
@@ -55,9 +57,44 @@ impl Zone {
             Vec::new()
         }
     }
+    pub fn get_accessible_cells(&self, start: Vec2, map: &Map, bombs: &[Bomb]) -> Vec<Vec2> {
+        if bombs.is_empty() {
+            // log::debug!("bomb empty !:accessible cells : {0:?}=", self.cells);
+            return self.cells.clone();
+        }
+        let mut accessible_cells = Vec::new();
+        let bombs_pos: Vec<Vec2> = bombs.iter().map(|bomb| bomb.position).collect();
+        self.get_accessible_cells_loop(start, &mut accessible_cells, map, &bombs_pos);
+        // log::debug!("accessible cells : {accessible_cells:?}=");
+        accessible_cells
+    }
 
-    pub fn check_bombs(&mut self) {
-        todo!("Bonjour")
+    fn get_accessible_cells_loop(
+        &self,
+        cell: Vec2,
+        accessible_cells: &mut Vec<Vec2>,
+        map: &Map,
+        bombs_pos: &Vec<Vec2>,
+    ) {
+        let neighbours: Vec<Vec2> = map
+            .get_neighbours(cell.grid())
+            .into_iter()
+            .filter(|neighbour| {
+                !accessible_cells.contains(&neighbour) && !bombs_pos.contains(&neighbour)
+            })
+            .collect();
+        for neighbour in neighbours {
+            accessible_cells.push(neighbour.grid());
+            self.get_accessible_cells_loop(neighbour, accessible_cells, map, bombs_pos);
+        }
+    }
+
+    pub fn check_dangerous_cells(&mut self, bombs: &[Bomb], map: &Map) -> Vec<Vec2> {
+        let mut dangerous_cells: Vec<Vec2> = Vec::new();
+        for bomb in bombs {
+            dangerous_cells.append(&mut bomb.get_explosion_cells(map))
+        }
+        dangerous_cells
     }
 
     // TODO: {loic} create generic function for getting closest position
@@ -106,18 +143,24 @@ impl Zone {
         players_position: &[Vec2],
         map: &Map,
     ) {
+        // log::debug!(
+        //     "== {} ==\ncells found during zone: {cells_found:?}, cells already in zone {2:?}, is player in zone ? {}\n",
+        //     start,
+        //     self.player_zone,
+        //     self.cells
+        // );
         let neighbours: Vec<Vec2> = map
-            .get_neighbours(start)
+            .get_neighbours(start.grid())
             .into_iter()
-            .filter(|neighbour| {
-                !cells_found.contains(&neighbour)
-            })
+            .filter(|neighbour| !cells_found.contains(&neighbour))
             .collect();
+        // log::debug!("cells to visit : {neighbours:?}");
         for neighbour in neighbours {
             if self.cells.contains(&neighbour) {
                 self.player_zone = true;
                 continue;
             }
+            // log::debug!("visiting neihggbour : {neighbour:?}");
             self.add_cell(
                 cells_found,
                 entities_found,
@@ -145,7 +188,7 @@ impl Zone {
         players_position: &[Vec2],
     ) {
         // log::debug!("added {cell} :)");
-        cells_found.push(cell);
+        cells_found.push(cell.grid());
         if let Some(player_id) = players_position.iter().position(|pos| *pos == cell) {
             let new_player = Entity::new(EntityType::Player(player_id as usize));
             if self.entities.iter().all(|entity| *entity != new_player) {
